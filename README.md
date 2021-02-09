be7c3b7591aecb2466f89a086a7a70a9e95ae6c6
moonburnt <moonburnt@disroot.org>
2021-02-09 19:26:32 +0200

    ability to verify zip-archived ROMs

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This is but **data verification tool**. It **does not and will never download an
 
 ## Description
 
-**Romman** (short of "rom manager". I dont like it too, if you can find a better name - make an issue) is a tool to verify hashes of your console ROM files and compare them with entries from datasheets, considered "the most accurate" by community (meaning if your rom didnt match any of these - then you probably dumped it incorrectly). Right now the following datasheet sources are supported: no-intro (only "Standard DAT"), tosec, redump, mame (only large xml from site, not separate data files from github).
+**Romman** (short of "rom manager". I dont like it too, if you can find a better name - make an issue) is a tool to verify hashes of your console ROM files and compare them with entries from datasheets, considered "the most accurate" by community (meaning if your ROM didnt match any of these - then you probably dumped it incorrectly). Right now the following datasheet sources are supported: no-intro (only "Standard DAT"), tosec, redump, mame (only large xml from site, not separate data files from github).
 
 ## Usage
 - Download up-to-date .dat files from your source of choice (supported providers listed in description above)
@@ -26,19 +26,20 @@ For complete list of currently available functionality run
 `./romman-cli -h`
 
 ## Currently implemented:
-- Compare roms with no-intro/tosec/redump .dat files or mame .xml (either placed into ./Datasheets directory or specified with --datfiles flag)
+- Compare ROMs with no-intro/tosec/redump .dat files or mame .xml (either placed into ./Datasheets directory or specified with --datfiles flag)
 - Log info about which ROM has matched which entry from which datasheet
 - Print total usage statistics at the end (e.g amount of hits, misses and files tool couldnt verify for whatever reasons) into stdout
 - Iterative datasheets parsing, so 250mbytes-large file wont eat all your ram
+- Ability to verify ROMs stored inside zip archives
 
 ## TODO:
 
 - Ability to fetch newest available datasheets
-- Verify games in zip archives
 - Delete non-matching files or move them to separate directory (in case they are archived - move whole archives)
 - Rename matching files with non-matching names (in case they are archived - skip)
-- Reduce ram usage even further (probably by providing hard limits on maximum database size and dumping it into cache file, if it gets to that point)
 - Log stdout to something like info.log and stderr to error.log
+- Maybe reduce ram usage even further (probably by providing hard limits on maximum database size and dumping it into cache file, if it gets to that point)
+- Maybe add ability to verify 7z-archived ROMs
 - Maybe make pyqt5 gui
 - Find a better name
 
--- romman/file_processing.py
+++ romman/file_processing.py
@@ -7,8 +7,10 @@
 # This module contains functions related to working with files
 
 import logging
+from zipfile import ZipFile, is_zipfile
 from os import listdir
-from os.path import isfile, isdir, join
+from os.path import isfile, isdir, join, basename
+from romman import hashcheck
 
 log = logging.getLogger(__name__)
 
@@ -39,3 +41,70 @@ def get_files(dirname):
 
     log.debug(f"Got following files in total: {files}")
     return files
+
+def get_zip_info(pathtofile):
+    '''Receives str(path/to/zipfile.zip. Return list containing dictionaries with each file's info - name, hashsum, path to archive and mention that its zip archive'''
+    log.debug(f"Attempting to parse zip archive: {pathtofile}")
+
+    datalist = []
+    with ZipFile(pathtofile, 'r') as zf:
+        for f in zf.infolist():
+            raw_crc = f.CRC
+            internal_path = f.filename
+            if raw_crc == 0:
+                log.debug(f'{internal_path} is directory or empty, skipping')
+                continue
+            else:
+                data = {}
+                #this is necessary coz file may be in subdirectory
+                data['name'] = basename(internal_path)
+                hex_crc = hex(raw_crc)
+                data['crc'] = hex_crc.replace('0x', '')
+                #this may be jank, but will do for log output
+                #which is the only thing why this entry exists anyway
+                data['path'] = join(pathtofile, internal_path)
+                data['is_zip'] = True
+
+                log.debug(f"Got following data: {data}")
+                datalist.append(data)
+
+    log.debug(f"Successfully fetched data from {pathtofile}, returning")
+    return datalist
+
+def get_file_info(pathtofile):
+    '''Receives str(path/to/file). Returns list containing dictionary with filename, hashsum, path to file and mention that its not zip archive'''
+    log.debug(f"Attempting to fetch info from {pathtofile}")
+
+    #List is completely unnecessary, but since zip files have these - I kinda have to follow same route
+    #I mean - its needed either there or within file processor. For now its there
+    datalist = []
+
+    crc = hashcheck.crc32sum(pathtofile)
+    if crc != '0': #its str coz crc32sum always returns str
+        data = {}
+        data['name'] = basename(pathtofile)
+        data['crc'] = crc
+        data['path'] = pathtofile
+        data['is_zip'] = False
+
+        log.debug(f"Got following data: {data}")
+        datalist.append(data)
+
+    #yeah, I know. If crc == 0, returning empty list. It will be .extended to other, regardless
+    log.debug(f"Successfully fetched data from {pathtofile}, returning")
+    return datalist
+
+
+def file_processor(pathtofile):
+    '''Receives str(path/to/file). Depending on if its zip or not - fetches info from it as archive or calculates manually. Return list with dictionary containing info about filepath, file's name and crc (or, in case its archive - names and crc of all files inside), also if its zip or not'''
+
+    log.debug(f"Determining filetype of {pathtofile}")
+    if is_zipfile(pathtofile):
+        log.debug(f"Seems like {pathtofile} is zip archive, proceeding accordingly")
+        data = get_zip_info(pathtofile)
+    else:
+        log.debug(f"{pathtofile} doesnt seem to be zip, threating as file")
+        data = get_file_info(pathtofile)
+
+    log.debug(f"Successfully gathered info about {pathtofile}, returning")
+    return data
